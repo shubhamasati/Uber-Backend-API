3e47976f8ae4ef3da59cc88a2cbad038dbdc0e37
Shubham Asati <shbham.asti128@gmail.com>
2020-11-03 00:04:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is Uber API Clone. Build using Spring Boot.
---
+++ src/test/java/com/hank/uber/UberapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hank.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
